b6a59c6e5037f72df7787c77582d2d8c4fc00d88
levabellee <levabelleedu@gmail.com>
2026-02-25 23:50:57 +0800

    Delete Lab6a directory

--- Lab6a/Task1.R
+++
@@ -1,7 +0,0 @@
-# Lab6b Task 1
-
-# Creating data frame
-quiz_details <- data.frame(name=c('Anastasia', 'Dima', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'),
-                            score=c(12.5, 9.0, 16.5, 12.0, 9.0, 8.0, 19.0),
-                            attempts=c(1, 3, 2, 3, 2, 1, 2))
-print(quiz_details)
