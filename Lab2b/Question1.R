68c31f862eb7ac31cd763808e854af4f5deff911
levabellee <levabelleedu@gmail.com>
2026-04-07 05:01:25 +0800

    Update Question1.R

--- Lab2b/Question1.R
+++ Lab2b/Question1.R
@@ -27,13 +27,13 @@ print(paste("Normal:", normal))
 print(paste("Overweight:", overweight))
 print(paste("Obese:", obese))
 
-/////////////
+// ================== //
   
 string1 <- readline(prompt="Enter string 1: ")
 string2 <- readline(prompt="Enter string 2: ")
 print(paste("This program compare 2 strings. Both inputs are similar: ",tolower(string1) == tolower(string2)))
 
-/////
+// ================== //
   
 name <- readline(prompt="Enter name: ")
 num <- readline(prompt="Enter phone number: ")
