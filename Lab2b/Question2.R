eab2d28ee3e4c65933c2c23c75b8db5a679ecea6
levabellee <levabelleedu@gmail.com>
2026-04-07 04:59:15 +0800

    Create Question2.R

---
+++ Lab2b/Question2.R
@@ -0,0 +1,3 @@
+string1 <- readline(prompt="Enter string 1: ")
+string2 <- readline(prompt="Enter string 2: ")
+print(paste("This program compare 2 strings. Both inputs are similar: ", tolower(string1) == tolower(string2)))
